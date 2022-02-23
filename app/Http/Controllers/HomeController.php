aad4cbf6f14d9fbb0b4c33670acbdd2cea128dd6
= <=>
2022-02-23 14:25:05 +0700

    js Praktikum2

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+public function index(){
+    echo "Ini adalah halaman home";
+}
+
+public function about(){
+    echo "NIM   : 2041720058 ";
+    echo "Nama  : Siti Zumaroh Dian Febrianti ";
+    echo "Kelas : TI 2D";
+}
+
+public function articles($id){
+    echo "Ini merupakan halaman dengan ID".$id;
+}
